126450efe2830d824dd92df2ed73ffd6572f2bf6
Abdulazeez Abdulsamad <abdulazeezabdulsamad16@gmail.com>
2023-11-22 07:46:20 +0100

    first

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { Inter } from 'next/font/google';
 import TrendingSlider from '@/components/Slider/TrendingSlider';
 import Download from '@/components/Download';
-import { Hero,Affordable,Ready,Unique,Testimony,Counter } from '@/components/home';
+import { Hero,Affordable,Ready,Unique,Testimony,Counter } from '../components/Home';
 const inter = Inter({ subsets: ['latin'] });
 export default function Home() {
    return (
