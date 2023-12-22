53be9602cd73bb1338a7642a1045a3d85fae6b2d
Abdulazeez Abdulsamad <abdulazeezabdulsamad16@gmail.com>
2023-12-22 23:43:37 +0100

    metas

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,18 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '@/layout/layout';
 import '@/components/Slider/TrendingSlider.css';
+import Head from 'next/head';
 export default function App({ Component, pageProps }: AppProps) {
    return (
-      <Layout>
+      <>
+      <Head>
+        <meta charSet='utf-8'/>
+         <title>Pusheat - Savoury Food Deals, Freshly Bidded By You</title>
+         </Head>
+       <Layout>
          <Component {...pageProps} />
       </Layout>
+      </>
+     
    );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,11 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <head>
-        <meta charSet='utf-8'/>
-         <title>Pusheat - Savoury Food Deals, Freshly Bidded By You</title>
-      </head>
+      <Head/>
       <body>
         <Main />
         <NextScript />
